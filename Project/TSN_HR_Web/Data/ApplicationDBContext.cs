243d85d4f8ad2e2017688c18584b9560c65bc557
ngoctran260202@gmail.com <ngoctran260202@gmail.com>
2025-12-02 16:53:56 +0700

    create new project

---
+++ Project/TSN_HR_Web/Data/ApplicationDBContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TSN_HR_Web.Models;
+
+namespace TSN_HR_Web.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+
+        // TODO: Thêm DbSet cho mỗi bảng
+        public DbSet<NhanVien> NhanViens { get; set; }
+    }
+}
+
+
