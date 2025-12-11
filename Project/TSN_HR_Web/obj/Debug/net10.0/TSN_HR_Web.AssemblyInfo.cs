661c773805f8075cff6aed58c9b83df1b4355686
ngoctran260202@gmail.com <ngoctran260202@gmail.com>
2025-12-11 17:02:47 +0700

    need to fix

--- Project/TSN_HR_Web/obj/Debug/net10.0/TSN_HR_Web.AssemblyInfo.cs
+++ Project/TSN_HR_Web/obj/Debug/net10.0/TSN_HR_Web.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TSN_HR_Web")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1511ebdd12a470de00900efaac6a2fb1acb8f9af")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b4cdb777cc3e109bb7431397fa25c9d58e0546de")]
 [assembly: System.Reflection.AssemblyProductAttribute("TSN_HR_Web")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TSN_HR_Web")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
